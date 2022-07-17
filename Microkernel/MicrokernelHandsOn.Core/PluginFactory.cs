4be938e4eb6d7467f5ac3940a25ded9317e2a0a8
Hoi Bui <44888397+hoibuikms@users.noreply.github.com>
2022-07-17 22:25:11 +0700

    Initial

---
+++ Microkernel/MicrokernelHandsOn.Core/PluginFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Reflection;
+using MicrokernelHandsOn.Contract;
+
+namespace MicrokernelHandsOn.Core
+{
+    public class PluginFactory
+    {
+        private readonly PluginConfigurationSection _pluginConfigurationSection;
+
+        public PluginFactory()
+        {
+            _pluginConfigurationSection = ConfigurationManager.GetSection("pluginSection") as PluginConfigurationSection;
+        }
+
+        public IEnumerable<IPlugin> GetPlugins()
+        {
+            var plugins = new List<IPlugin>();
+
+            foreach (PluginElement plugin in _pluginConfigurationSection.PluginElementCollection)
+            {
+                var type = Type.GetType(plugin.Type);
+
+                if (type == null)
+                    continue;
+
+                var pluginInstance1 = Activator.CreateInstance(type) as IPlugin;
+
+                if (pluginInstance1 == null)
+                    continue;
+                plugins.Add(pluginInstance1);
+            }
+            return plugins;
+        }
+    }
+}
